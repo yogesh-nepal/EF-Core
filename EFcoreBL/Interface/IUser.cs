98c7bb3234750cc93e38df1b257f5ae95621aeff
yoges nepal <yogesn2@hotmail.com>
2020-05-10 09:14:30 +0545

    Join User and Role Table

--- EFcoreBL/Interface/IUser.cs
+++ EFcoreBL/Interface/IUser.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
 using EFmodels;
 
 namespace EFcoreBL.Interface
 {
     public interface IUser : IGenericRepository<UserModel>
     {
-         
+         IEnumerable<UserModel> GetUserWithRole();
     }
 }
--- EFcoreBL/Repository/UserRepository.cs
+++ EFcoreBL/Repository/UserRepository.cs
@@ -1,16 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
 using EFcoreBL.Interface;
 using EFcoreDAL;
 using EFmodels;
 
 namespace EFcoreBL.Repository
 {
-    public class UserRepository : GenericRepository<UserModel>,IUser
+    public class UserRepository : GenericRepository<UserModel>, IUser
     {
         private readonly DatabaseContext databaseContext;
 
-        public UserRepository(DatabaseContext _databaseContext):base(_databaseContext)
+        public UserRepository(DatabaseContext _databaseContext) : base(_databaseContext)
         {
             databaseContext = _databaseContext;
+        }
+
+        public IEnumerable<UserModel> GetUserWithRole()
+        {
+            var data = from u in databaseContext.UserTable.ToList()
+                       join r in databaseContext.RoleTable.ToList()
+                       on u.RoleID equals r.RoleID
+                       select new UserModel()
+                       {
+                           roles = r,
+                           UserID = u.UserID,
+                           UserFullName = u.UserFullName,
+                           UserEmailID = u.UserEmailID,
+                           UserAddress = u.UserAddress,
+                           UserGender = u.UserGender,
+                           UserPhone = u.UserPhone,
+                           IsActive = u.IsActive
+                       };
+            return data;
         }
     }
 }
