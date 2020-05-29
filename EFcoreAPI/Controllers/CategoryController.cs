e04500d0e5ebee0e9bd131f04beeb6dcdc102c19
yoges nepal <yogesn2@hotmail.com>
2020-05-29 21:28:50 +0545

    Insert, Delete and Show Categories

---
+++ EFcoreAPI/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EFcoreBL.Interface;
+using EFmodels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EFcoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategory iCategory;
+        public CategoryController(ICategory _iCategory)
+        {
+            iCategory = _iCategory;
+        }
+
+        [HttpPost("Insert")]
+        [Authorize(Roles="Admin")]
+        public IActionResult Insert(CategoryModel model)
+        {
+            iCategory.InsertIntoTable(model);
+            iCategory.Save();
+            return Ok(HttpStatusCode.OK);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        [Authorize(Roles="Admin")]
+        public IActionResult Delete(int id)
+        {
+            iCategory.DeleteFromTable(id);
+            iCategory.Save();
+            return Ok(HttpStatusCode.OK);
+        }
+
+        [HttpGet("ShowAll")]
+        public IActionResult ShowAll()
+        {
+            var list = iCategory.GetAllFromTable();
+            return Ok(list);
+        }
+    }
+}
